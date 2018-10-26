692953b21e181d30d8699592ffd44e0f0f470625
Dominik Ampletzer <dominikampletzer@yahoo.de>
2018-10-26 11:54:25 +0200

    Ascii Doc aufsetzten

--- functionalComposition/funcitonalComposition_test.go
+++ functionalComposition/funcitonalComposition_test.go
@@ -8,4 +8,15 @@ import (
 func TestFunctionalComposition(t *testing.T) {
 	var c = compose(square, square)(2)
 	fmt.Println(c)
+
+	a := intSequence()
+	fmt.Println(a())
+	fmt.Println(a())
+	fmt.Println(a())
+	fmt.Println(a())
+
+	b := intSequence()
+	fmt.Println(b())
+	fmt.Println(a())
+	fmt.Println(b())
 }
